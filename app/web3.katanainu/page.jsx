34f786d0b7557af0439c5c1af6b86579dc1b5476
Katana Inu <dev@katanainu.com>
2026-04-03 16:32:53 +0200

    Update page.jsx

--- app/web3.katanainu/page.jsx
+++ app/web3.katanainu/page.jsx
@@ -6,7 +6,7 @@ import BrandLogo from "@/components/pages/home/BrandLogo";
 export default function Web3Katana() {
   return (
     <main className="overflow-hidden">
-      <div className="bg-[url('/assets/images/background.jpg')] bg-[0%_25%] bg-cover z-[1] relative brand_container">
+      <div className="bg-[url('/assets/images/background.jpg')] bg-[0%_25%] bg-cover z-[1] relative brand_container pt-20 md:pt-24">
         <BrandLogo />
       </div>
       <TokenOverview />
