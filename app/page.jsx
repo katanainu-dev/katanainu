f6dcf0d36de482dc77ef2d1889a30094763f9561
Katana Inu <dev@katanainu.com>
2026-04-03 13:23:31 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 //import Bannner from "@/components/pages/home/Bannner";
 import Brand from "@/components/pages/home/Brand";
-import BrandLogo from "@/components/pages/home/BrandLogo";
 import ChoosePlayer from "@/components/pages/home/ChoosePlayer";
 //import GameCards from "@/components/pages/home/Gamecard";
 import Newsletter from "@/components/pages/home/Newsletter";
